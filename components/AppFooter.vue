4cd3114c5e0c313f7261e8310063fdb979209a25
achraffawzi <achraffawzi224a@gmail.com>
2021-10-13 22:36:59 +0200

    Adding footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-footer">
+    <v-footer class="black">
+      <v-container>
+        <p class="white--text text-center footer-content">&copy; {{ getCurrentYear }} Copyright - All rights reserved to Blogg community</p>
+      </v-container>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AppFooter",
+
+    computed: {
+      getCurrentYear() {
+        return new Date().getFullYear();
+      },
+    },
+  }
+</script>
+
+<style scoped>
+.footer-content {
+  font-size: 14px !important;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
         <Nuxt />
       </v-container>
     </v-main>
+    <AppFooter />
   </v-app>
 </template>
 
