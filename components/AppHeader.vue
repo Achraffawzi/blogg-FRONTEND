3b1a455f716421b6e74981599e594e9dfb651965
achraffawzi <achraffawzi224a@gmail.com>
2021-10-13 22:24:15 +0200

    Creating header

---
+++ components/AppHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="app-header black">
+    <v-container class=" white--text">
+      <v-row class="align-center">
+        <v-col cols="12" sm="6" class="text-right text-h4 branding">
+          Blogg
+        </v-col>
+        <v-col cols="12" sm="6" class="text-right">
+          <nuxt-link to="#" class="header-link white--text text-decoration-none mr-5">Sign up</nuxt-link>
+          <nuxt-link to="#" class="header-link white--text text-decoration-none">Register</nuxt-link>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "AppHeader"
+  }
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Rancho&display=swap');
+
+.branding { font-family: 'Rancho', cursive !important }
+
+.header-link {
+  font-size: 14px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    homepage
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Index"
+  }
+</script>
+
+<style scoped>
+
+</style>
